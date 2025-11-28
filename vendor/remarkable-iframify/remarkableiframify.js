56144dbe4347d34479d1daa62aa75264ebc03be7
Uyuvnp Tcan <uyuvnp@tauznn.me>
2025-11-28 19:20:08 +0000

    Use Invidious for playing Youtube videos

--- vendor/remarkable-iframify/remarkableiframify.js
+++ vendor/remarkable-iframify/remarkableiframify.js
@@ -19,7 +19,7 @@
             closeTag: () => '</iframe></div>'
         },
         'youtube': {
-            openTag: (videoId) => '<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.2493%;"><iframe src="https://www.youtube.com/embed/' + videoId + '?rel=0&showinfo=0" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen scrolling="no" allow="autoplay; encrypted-media">',
+            openTag: (videoId) => '<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.2493%;"><iframe src="https://iv.nboeck.de/embed/' + videoId + '?rel=0&showinfo=0" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen scrolling="no" allow="autoplay; encrypted-media">',
             closeTag: () => '</iframe></div>'
         },
         'youtube-playlist': {
